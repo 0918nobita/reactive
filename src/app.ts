bbf0f0e43ef506c616496bd921802d9509dec415
0918nobita <nobita.0918@gmail.com>
2018-10-10 02:11:36 +0900

    :sparkles: Subscriber を作成して値を流すサンプルを追加した

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+import { Subscriber } from 'rxjs';
+
+// Subscriber の作成
+const subscriber = new Subscriber<number>(
+    (value) => console.log('Next: ' + value),
+    (error) => console.error(error),
+    () => console.log('Completed.'));
+
+subscriber.next(1);
+// subscriber.error(new Error('いyarn'));
+subscriber.complete();
+subscriber.unsubscribe();
+subscriber.next(2);
