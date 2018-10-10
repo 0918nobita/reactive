9ecc604552b896977e3ca7923f2e7f745f04711d
0918nobita <nobita.0918@gmail.com>
2018-10-10 11:25:17 +0900

    :sparkles: TeardownLogic を用いたサンプルを追加した

--- src/app.ts
+++ src/app.ts
@@ -12,3 +12,22 @@ subscriber.next(2);
 // subscriber.complete();
 // subscriber.unsubscribe();
 subscriber.next(3);
+
+/*
+  TeardownLogic
+  This interface describes what should be returned by function passed to Observable constructor or static create function.
+  Value of that interface will be used to cancel subscription for given Observable.
+
+  TeardownLogic can be:
+
+  1. Function
+    Function that takes no parameters. When consumer of created Observable calls unsubscribe, that function will be called
+  2. AnonymousSubscription
+    AnonymousSubscription is simply an object with unsubscribe method on it. That method will work the same as function
+  3. void
+    If created Observable does not have any resources to clean up, function does not have to return anything.
+ */
+
+subscriber.add(() => {
+  console.log('Teardown Logic');
+});
